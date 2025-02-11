8fb4a3691831b4da892501391c0ad186d50c3c4e
asadbekzaynobiddinov <asadbekzaynobiddinov51@gmail.com>
2025-02-11 18:02:56 +0500

    Update LastMessageGuar

--- src/common/guard/lastMessage.guard.ts
+++ src/common/guard/lastMessage.guard.ts
@@ -3,6 +3,8 @@ import { Telegraf } from 'telegraf';
 import { config } from 'src/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersEntity, UsersRepository } from 'src/core/';
+import { lastMessgeText } from '../constants';
+import e from 'express';
 
 @Injectable()
 export class LastMessageGuard implements CanActivate {
@@ -16,13 +18,17 @@ export class LastMessageGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const [ctx] = context.getArgs();
+    const user = await this.userRepo.findOne({
+      where: { telegram_id: `${ctx.from.id}` },
+    });
     if (
       ctx.session.lastMessage &&
       ctx.update.callback_query.message.message_id !=
         ctx.session.lastMessage.message_id
     ) {
-      await ctx.deleteMessage(ctx.update.callback_query.message.message_id);
-      return false;
+      await ctx.answerCbQuery(lastMessgeText[user.lang], ctx.callbackQuery.id);
+      await ctx.deleteMessage(ctx.update.callback_query.message.message_id)
+      return;
     }
     return true;
   }
