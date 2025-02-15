3af3bb1dbee73ce84801e2fdb5bc72cbdafcb4fa
muhiddinovismoil <muhiddinovismoil2@gmail.com>
2025-02-15 13:49:16 +0500

    env configuration updated and some secret things moved to it

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,10 +5,14 @@ export type ConfigType = {
   PORT: number;
   DB_URL: string;
   TOKEN: string;
+  TG_CHANNEL: string;
+  BOT_URL: string;
 };
 
 export const config: ConfigType = {
   PORT: Number(process.env.PORT) as number,
   DB_URL: process.env.DB_URL as string,
   TOKEN: process.env.TOKEN,
+  TG_CHANNEL: process.env.TG_CHANNEL as string,
+  BOT_URL: process.env.BOT_URL as string,
 };
